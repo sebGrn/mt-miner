55de36a7fba8811d0983d239a1c678250cc9ab02
Sébastien GERIN <sebastien.gerin@sayens.fr>
2021-03-25 00:10:51 +0100

    add Dockerfile with a volume

---
+++ Dockerfile
@@ -0,0 +1,57 @@
+# -------------------------------------------------------------------------------------------------- #
+# get opencascade code source and compil it 
+FROM ubuntu:18.04
+
+WORKDIR /
+
+# get dependencies packages
+# python3 : python library
+RUN apt-get update && apt install -y git cmake wget gcc g++
+RUN apt install -y  python3-pip
+
+# -------------------------------------------------------------------------------------------------- #
+# copy miner code 
+
+WORKDIR /
+
+RUN mkdir app
+RUN mkdir app/mt-miner/
+
+# copy all directory into container
+COPY MT-Miner/*.cpp app/MT-Miner/
+COPY MT-Miner/*.h app/MT-Miner/
+COPY MT-Miner/CMakeLists.txt app/MT-Miner/
+
+
+# -------------------------------------------------------------------------------------------------- #
+# copy python script files
+
+WORKDIR /
+
+RUN mkdir /app/scripts/
+
+# copy all directory into  container
+COPY scripts/*.py /app/scripts/
+
+# -------------------------------------------------------------------------------------------------- #
+# -------------------------------------------------------------------------------------------------- #
+# build server API
+
+WORKDIR /app/scripts
+
+# install python dependencies
+COPY scripts/requirements.txt .
+RUN pip3 install -r requirements.txt
+
+WORKDIR /
+RUN mkdir app/data/
+
+WORKDIR /app/scripts
+CMD ["python3", "./launch.py", "../data"]
+#ENTRYPOINT [ "python3", "./launch.py"]
+
+# docker build -t miner . 
+# docker run -v d:\dev\repository\MT-Miner\data:/app/data miner
+# docker run -v /home/sebastien.gerin/miner/compare_shd_miner/data:./app/data miner
+
+# docker run -it miner ../data/
--- scripts/launch.py
+++ scripts/launch.py
@@ -2,13 +2,13 @@ import subprocess
 import sys
 import os
 import pandas as pd
-from numpy import nan as Nan
 import time
 from time import perf_counter
+from shutil import copyfile
 
 import compil as cp
 
-def compare_shd_miner(dataset_folder, cpp_log_file, shd_log_file):
+def run_miner(dataset_folder, cpp_log_file):
     onlyfiles = [f for f in os.listdir(dataset_folder) if os.path.isfile(os.path.join(dataset_folder, f))]
     if(len(onlyfiles) == 0):
         print("no dataset in ", dataset_folder)
@@ -18,11 +18,7 @@ def compare_shd_miner(dataset_folder, cpp_log_file, shd_log_file):
         cpp_log_file = os.path.join(current_path, cpp_log_file)
         if os.path.isfile(cpp_log_file):
             os.remove(cpp_log_file)
-                
-        shd_log_file = os.path.join(current_path, shd_log_file)
-        if os.path.isfile(shd_log_file):
-            os.remove(shd_log_file)
-        
+                        
         for file in onlyfiles:
             if file.lower().endswith(('.txt', '.dat')):
                 fullpath = os.path.join(dataset_folder, file)
@@ -30,6 +26,8 @@ def compare_shd_miner(dataset_folder, cpp_log_file, shd_log_file):
                 print("processing data file:", fullpath)
                 
                 print("Miner execution...")
+                print(os.getcwd())
+
                 os.chdir("../MT-Miner")
                 start = perf_counter()            
                 cp.compil_and_run_miner(fullpath, cpp_log_file)
@@ -41,15 +39,24 @@ def compare_shd_miner(dataset_folder, cpp_log_file, shd_log_file):
         os.chdir(current_path)
 
         df_cpp = pd.read_csv(cpp_log_file, delimiter=';', header=None)
-        df_cpp.columns = ['filename', 'sparcity', 'cloneCount', 'itemCount', 'objectCount', 'minerMinimalTrasverseCount', 'minimalSizeOfTrasverse', 'minerTime']
+        df_cpp.columns = ['filename', 'sparcity', 'cloneCount', 'itemCount', 'objectCount', 'minerMinimalTransverseCount', 'minimalSizeOfTransverse', 'minerTime']
         df_cpp.index = df_cpp["filename"]
         df_cpp.index.name = "filename"
         #df = df.drop(["filename"], axis=1)
         df_cpp.to_csv(cpp_log_file, sep=';')
 
-        print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
-        print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
-        
+
+def run_shd(dataset_folder, shd_log_file):
+    onlyfiles = [f for f in os.listdir(dataset_folder) if os.path.isfile(os.path.join(dataset_folder, f))]
+    if(len(onlyfiles) == 0):
+        print("no dataset in ", dataset_folder)
+    else:        
+        current_path = os.getcwd()
+
+        shd_log_file = os.path.join(current_path, shd_log_file)
+        if os.path.isfile(shd_log_file):
+            os.remove(shd_log_file)
+
         data_shd = []  
         for file in onlyfiles:
             if file.lower().endswith(('.txt', '.dat')):
@@ -74,7 +81,7 @@ def compare_shd_miner(dataset_folder, cpp_log_file, shd_log_file):
         
         os.chdir(current_path)
 
-        df_shd = pd.DataFrame(data_shd, columns=['filename', 'shdMinimalTrasverseCount', 'shdTime'])
+        df_shd = pd.DataFrame(data_shd, columns=['filename', 'shdMinimalTransverseCount', 'shdTime'])
         df_shd.index = df_shd["filename"]
         df_shd.index.name = "filename"
         df_shd.to_csv(shd_log_file, sep=';')
@@ -85,7 +92,7 @@ if(len(sys.argv) != 2):
 else:
     dataset_folder = str(sys.argv[1])
 
-    cpp_log_file = "cpp_log.csv"
-    shd_log_file = "shd_log.csv"
+    run_miner(dataset_folder, "cpp_log.csv")
+    copyfile("cpp_log.csv", "../data/cpp_log.csv")
+    #run_shd(dataset_folder, "shd_log_file")
 
-    compare_shd_miner(dataset_folder, cpp_log_file, shd_log_file)
